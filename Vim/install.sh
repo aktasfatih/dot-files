221237c11c232aa5eda44f2c89f1f730fa81b95d
Fatih Aktas <akfatih2@gmail.com>
2022-09-06 14:31:28 +0000

    Add Vim script

--- Vim/install.sh
+++ Vim/install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Install Vundle
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
+then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+fi
+
+# Copy the vim rc
 ln -s -F $(pwd)/.vimrc ~/.vimrc
+
+echo "You need to run PluginInstall inside vim";
